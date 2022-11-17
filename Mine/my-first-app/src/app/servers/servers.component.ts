b2711bb94a0abd53be099f386843daf697bf6730
aaeshaker <aaeshaker@outlook.com>
2022-11-17 16:05:33 +0200

    Till Two-Way-Databinding in basics

--- Mine/my-first-app/src/app/servers/servers.component.ts
+++ Mine/my-first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created!';
+  serverName: string = '';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    /*
+    This (<HTMLInputElement>event.target) used to inform TypeScript that we know that
+     the type of HTML element of this event will be an HTML input element.
+     We do this explicit casting with this syntax.
+     */
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
